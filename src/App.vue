9b5332700c99cb2aa90ba3d4b07a487426ae6206
viveksharma <viveks_@hotmail.com>
2023-01-02 14:32:16 -0800

    added router functionality to the web app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 //1. Create a public github repo for this project, and commit everyday to get green squares (Done)
-//1.1 pull from the repo because I changed the readme, and then push to the repo, and continue with adding the router
+//1.1 pull from the repo because I changed the readme, and then push to the repo, and continue with adding the router (Done)
 //2. Setup vue router, maybe with a whole new project setup, or integrate it into here. 
 //3. Put the project up on AWS Amplify (or google or Azure - just for learning's sake)
 
 
 <template>
   <div>
-    <HelloWorld msg="Vite + Vue" />
-
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default{
+  
+}
+
 </script>
 
 <style scoped>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>Hello, this is the About Page</h3>
+    
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
