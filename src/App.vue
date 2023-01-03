469bc794314370b92bd554bef7793481c29c39e9
viveksharma <viveks_@hotmail.com>
2023-01-02 16:28:00 -0800

    added comments regarding next steps

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 //1. Create a public github repo for this project, and commit everyday to get green squares (Done)
 //1.1 pull from the repo because I changed the readme, and then push to the repo, and continue with adding the router (Done)
-//2. Setup vue router, maybe with a whole new project setup, or integrate it into here. 
-//3. Put the project up on AWS Amplify (or google or Azure - just for learning's sake)
+//2. Setup vue router, maybe with a whole new project setup, or integrate it into here. (Done)
+//3. Put the project up on AWS Amplify (or google or Azure - just for learning's sake) (Done)
+//4. Figure out how to store images online, and access them in the app
+//5. Keep working on app, create new pages that detail an image, an so on, when clicked on
 
 
 <template>
   <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link> | 
+    <router-link to="/about">About</router-link> 
   </div>
   <router-view/>
 </template>
 
 <script>
 export default{
-  
+
 }
 
 </script>
